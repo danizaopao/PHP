bbee7bfce10e83e47d4d83b580c6929935cc529b
danizaopao <daniel.oliveira.senai.404@gmail.com>
2024-08-23 09:16:39 -0300

    Add files via upload

---
+++ php/foratividade.php
@@ -0,0 +1,10 @@
+<?php
+//definir a variavel da soma como 0 para somar tudo depois
+$soma=0;
+//utilização do for para o cálculo de todos os numeros de 1 a 100
+for($i=1;$i<=100;$i++){
+    $soma +=$i;
+}
+//exiibição do resultado da soma total
+echo "A soma de todos os numeros é: $soma <br>";
+?>
