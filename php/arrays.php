1639d12356d8fddc7c3ae4add4ecc27ef61b9ab8
danizaopao <daniel.oliveira.senai.404@gmail.com>
2024-08-28 15:38:12 -0300

    Add files via upload

---
+++ php/arrays.php
@@ -0,0 +1,38 @@
+<?php
+// //criação da lista numerada
+// $lista_frutas=array("Banana","Maça","Morango","Uva");
+// echo '<pre>';
+// print_r($lista_frutas);
+// echo $lista_frutas[0];
+// echo "<hr>";
+// echo "</pre>";
+
+// echo '<pre>';
+// //alterando o valor 0 -> Banana para Melancia
+// $lista_frutas[0]="Melancia";
+// print_r ($lista_frutas);
+// echo '</pre>';
+
+// echo "<br>";
+
+// echo $lista_frutas;
+
+
+$numeros=[1,2,3,4,5,6,7,8,9,10];
+
+echo '<pre>';
+print_r($numeros);
+echo '</pre>';
+echo '<br>';
+echo $numeros[0]=100;
+echo '<br>';
+echo $numeros[4]=500;
+echo '<br>';
+echo $numeros[9]=1000;
+echo '<br>';
+echo '<hr>';
+echo '<pre>';
+print_r($numeros);
+echo $numeros;
+echo '</pre';
+?>
